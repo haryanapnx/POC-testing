e7c57e6ac7ba4e3bd1817fe2bcf2e46479f84ce7
haryanapnx <hary.vai@gmail.com>
2022-09-21 22:23:37 +0700

    fix(AddPost): set data the response into state

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -20,7 +20,7 @@ function AddPost() {
         { title, body, userId },
         { headers: { "Content-type": "application/json; charset=UTF-8" } }
       );
-      alert(JSON.stringify(response.data));
+      setData(response.data);
     } catch (err) {
       console.error(err.message);
     } finally {
@@ -29,45 +29,55 @@ function AddPost() {
   };
 
   return (
-    <form
-      style={{
-        padding: "2rem",
-        border: ".5px solid #ccc",
-        margin: "4rem 2rem",
-      }}
-      onSubmit={handleSubmit}
-      data-testid="my-form"
-    >
-      <h4>Add Post</h4>
-      <Input
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        required
-        name="title"
-        data-testid="title"
-        label="Title"
-      />
-      <Input
-        onChange={(e) => setBody(e.target.value)}
-        value={body}
-        required
-        name="body"
-        data-testid="body"
-        label="Body"
-      />
-      <Input
-        onChange={(e) => setUserId(e.target.value)}
-        value={userId}
-        type="number"
-        required
-        name="userId"
-        data-testid="userId"
-        label="User ID"
-      />
-      <MyButton data-testid="add" type="submit" disabled={loading}>
-        {loading ? "loading..." : "ADD"}
-      </MyButton>
-    </form>
+    <>
+      <form
+        style={{
+          padding: "2rem",
+          border: ".5px solid #ccc",
+          margin: "4rem 2rem",
+        }}
+        onSubmit={handleSubmit}
+        data-testid="my-form"
+      >
+        <h4>Add Post</h4>
+        <Input
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+          name="title"
+          data-testid="title"
+          label="Title"
+        />
+        <Input
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+          required
+          name="body"
+          data-testid="body"
+          label="Body"
+        />
+        <Input
+          onChange={(e) => setUserId(e.target.value)}
+          value={userId}
+          type="number"
+          required
+          name="userId"
+          data-testid="userId"
+          label="User ID"
+        />
+        <MyButton data-testid="add" type="submit" disabled={loading}>
+          {loading ? "loading..." : "ADD"}
+        </MyButton>
+        {data && (
+          <div data-testid="result-data">
+            <h4>Data berhasil disimpan</h4>
+            <pre style={{ border: "dashed 1px orange", padding: ".5rem" }}>
+              {JSON.stringify(data, undefined, 2)}
+            </pre>
+          </div>
+        )}
+      </form>
+    </>
   );
 }
 
